6bb21383166b7cb3955e90b283e220735015a47d
vav <vavvav@i.ua>
2024-05-20 21:19:28 +0300

    slice-use-layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,71 +1,111 @@
 "use client";
+// удалено ч2 01-47-43  import type { Metadata } from "next";
+//удалено import { Inter } from "next/font/google";
+ 
+import "./globals.css";
+  import { Poppins } from "next/font/google";
+  import { Josefin_Sans } from "next/font/google";
 
-import React, { FC, useEffect, useState } from "react";
- import "./globals.css";
-import { Poppins } from "next/font/google";
-                     import { Toaster } from "react-hot-toast";
-import { Josefin_Sans } from "next/font/google";
-import { Providers } from "./Provider";
-import { ThemeProvider } from "./utils/theme-provider"; //импортируем нашу тем
+//импортируем нашу тему
+import { ThemeProvider } from "./utils/theme-provider";
+ import { Toaster } from "react-hot-toast";
 
-import { useLoadUserQuery } from "@/redux/features/api/apiSlice";
-import Loader from "./components/Loader/Loader";
-
+  import { Providers } from "./Provider"; // ч2 01-47-43 подключил
+  import { SessionProvider } from "next-auth/react";
+  import React, { FC, useEffect, useState } from "react";
+  
+   import { useLoadUserQuery, useRefreshTokenQuery } from "@/redux/features/api/apiSlice";
+  import Loader from "./components/Loader/Loader";
+ 
+  // import socketIO from "socket.io-client";
+  // const ENDPOINT = process.env.NEXT_PUBLIC_SOCKET_SERVER_URI || "";
+  // const socketId = socketIO(ENDPOINT, { transports: ["websocket"] }); //индетификатор сокета
 
 
 //добавили шрифты (использ они в tailwind.config.ts)
-const poppins = Poppins({
-  subsets: ["latin"],
-   weight: ["400", "500", "600", "700"],
-   variable: "--font-Poppins",
- });
-
- const josefin = Josefin_Sans({
+  const poppins = Poppins({
    subsets: ["latin"],
-   weight: ["400", "500", "600", "700"],
-  variable: "--font-Josefin",
- });
+    weight: ["400", "500", "600", "700"],
+    variable: "--font-Poppins",
+  });
+
+  const josefin = Josefin_Sans({
+    subsets: ["latin"],
+    weight: ["400", "500", "600", "700"],
+   variable: "--font-Josefin",
+  });
 
 //глобальный макет для страниц
 
-
 export default function RootLayout({
-  children 
- }:{ children: React.ReactNode
- }) {
- return (
-   <html lang="en"           
-   suppressHydrationWarning={true}
-   >
-    <body className={`${poppins.variable} ${josefin.variable} !bg-white bg-no-repeat 
-      dark:bg-gradient-to-b  dark:from-gray-900 dark:to-black duration-300`
-     }>   
-        <Providers>
+   children, 
+  }:{ children: React.ReactNode
+  }) {
+  return (
+    <html lang="en"           
+    suppressHydrationWarning={true}
+    >
+     <body className={`${poppins.variable} ${josefin.variable} !bg-white bg-no-repeat 
+       dark:bg-gradient-to-b  dark:from-gray-900 dark:to-black duration-300`
+      }>   
       
-      <ThemeProvider attribute= 'class'  defaultTheme="light" > 
-      {/* defaultTheme='system' enableSystem={true}  > */}
-      
-       {/* <Custom>  <div>{children}</div>  </Custom> */}
-      {children}  
 
-      <Toaster position="top-center" reverseOrder={false} />
-
-      </ThemeProvider>
-
-      </Providers>
-      </body>
-    </html>
-  );
-}
-
-
-
-// const Custom: FC<{ children: React.ReactNode }> = ({ children }) => {
+      <Providers>  {/* подключили store*/}
  
-//   const { isLoading, error } = useLoadUserQuery({});
-
-
+ {/* Этот провайдер контекста позволяет вашему приложению получить 
+    доступ к данным сессии из любого места вашего приложения,  не 
+ передавая их как пропсы  => const { data: session } = useSession(); */}
+              <SessionProvider> 
+ {/* наша тема  */}
   
+ <ThemeProvider attribute= 'class'  defaultTheme="light"  > 
+      {/*    attribute= '  class  '  defaultTheme='system' enableSystem   > */}
+   
+ <Custom>{children}</Custom>    
+  {/* <Custom><div>{children}</div> </Custom>      */}
+               {/* {children}   */}
+         
+            <Toaster position="top-center" reverseOrder={false} />     
+             
+             </ThemeProvider>  
+          </SessionProvider > 
+          </Providers>    
+        
+       </body>
+    </html>
+  ) 
+}
  
-//   return <>{isLoading ? <Loader /> : <div>{children}</div>}</>;
-// };
+  const Custom: FC<{ children: React.ReactNode }> = ({ children }) => {
+    
+  // пока нет  const {   isLoading  } = useLoadUserQuery({});
+  
+   
+
+    //  useEffect(() => {
+ 
+    //    socketId.on("connection", () => {}) //подключаемся
+    //  }, []);
+
+ 
+
+
+ // console.log(  '======== итак children='  )   
+
+    // return ( 
+    //   <>{isLoading ? <Loader /> : <div>{children}</div>}</> 
+    // )
+    return ( 
+      <>{  <div>{children}</div>}</> 
+    )
+  } 
+
+ /**
+   * 
+  * Чтобы отобразить индикатор загрузки для фоновой загрузки, вы можете
+  *  посмотреть значение, isFetching перенастроенное из useQuery, 
+  * которое будет true всякий раз, когда происходит какая-либо выборка,
+  *  независимо от того, первая это выборка или повторная выборка. 
+  * Вы также можете использовать isRefetching только проверку наличия 
+  * повторных выборок.
+    */
---
+++ app/oldlayout.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import React, { FC, useEffect, useState } from "react";
+ import "./globals.css";
+import { Poppins } from "next/font/google";
+                     import { Toaster } from "react-hot-toast";
+import { Josefin_Sans } from "next/font/google";
+import { Providers } from "./Provider";
+import { ThemeProvider } from "./utils/theme-provider"; //импортируем нашу тем
+
+import { useLoadUserQuery } from "@/redux/features/api/apiSlice";
+import Loader from "./components/Loader/Loader";
+
+
+
+//добавили шрифты (использ они в tailwind.config.ts)
+const poppins = Poppins({
+  subsets: ["latin"],
+   weight: ["400", "500", "600", "700"],
+   variable: "--font-Poppins",
+ });
+
+ const josefin = Josefin_Sans({
+   subsets: ["latin"],
+   weight: ["400", "500", "600", "700"],
+  variable: "--font-Josefin",
+ });
+
+//глобальный макет для страниц
+
+
+export default function RootLayout({
+  children 
+ }:{ children: React.ReactNode
+ }) {
+ return (
+   <html lang="en"           
+   suppressHydrationWarning={true}
+   >
+    <body className={`${poppins.variable} ${josefin.variable} !bg-white bg-no-repeat 
+      dark:bg-gradient-to-b  dark:from-gray-900 dark:to-black duration-300`
+     }>   
+        <Providers>
+      
+      <ThemeProvider attribute= 'class'  defaultTheme="light" > 
+      {/* defaultTheme='system' enableSystem={true}  > */}
+      
+       {/* <Custom>  <div>{children}</div>  </Custom> */}
+      {children}  
+
+      <Toaster position="top-center" reverseOrder={false} />
+
+      </ThemeProvider>
+
+      </Providers>
+      </body>
+    </html>
+  );
+}
+
+
+
+// const Custom: FC<{ children: React.ReactNode }> = ({ children }) => {
+ 
+//   const { isLoading, error } = useLoadUserQuery({});
+
+
+  
+ 
+//   return <>{isLoading ? <Loader /> : <div>{children}</div>}</>;
+// };
