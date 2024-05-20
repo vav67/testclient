d6355f0d3dbce88b29554fad8d84e4bbc0d4083a
vav <vavvav@i.ua>
2024-05-20 19:35:02 +0300

    menu- app=abouy=faq

---
+++ app/policy/Policy.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { styles } from "../styles/style";
+
+type Props = {};
+
+const Policy = (props: Props) => {
+  return (
+    <div>
+      <div className={"w-[95%] 800px:w-[92%] m-auto py-2  text-black  dark:text-white px-3"}>
+        <h1 className={`${styles.title} !text-start pt-2`}>
+        Условия использования платформы  Platform Terms and Condition
+        </h1>
+      <ul style={{ listStyle: "unset", marginLeft: "15px" }}>
+      <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
+      Заказчику очень важно обратить внимание на процесс адиписки. Сделать так, чтобы лесть
+       зодчего мешала в сказанном, но и более грубые удовольствия в противном случае легко 
+       принять от блаженных, вина самых достойных обвинителей в том, что они никогда не 
+       страдают! Заказчику очень важно обратить внимание на процесс адиписки. Сделать так,
+        чтобы лесть зодчего мешала в сказанном, но и более грубые удовольствия в противном 
+        случае легко принять от блаженных, вина самых достойных обвинителей в том, что 
+        они никогда не страдают!
+        </p>
+        <br />
+        <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+          blanditiis architecto quasi impedit in dicta nisi, asperiores
+          voluptatum eos alias facilis assumenda ex beatae, culpa dignissimos
+          accusantium quod numquam dolores! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+          blanditiis architecto quasi impedit in dicta nisi, asperiores
+          voluptatum eos alias facilis assumenda ex beatae, culpa dignissimos
+          accusantium quod numquam dolores!
+        </p>
+        <br />
+        <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+          blanditiis architecto quasi impedit in dicta nisi, asperiores
+          voluptatum eos alias facilis assumenda ex beatae, culpa dignissimos
+          accusantium quod numquam dolores! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+          blanditiis architecto quasi impedit in dicta nisi, asperiores
+          voluptatum eos alias facilis assumenda ex beatae, culpa dignissimos
+          accusantium quod numquam dolores!
+        </p>
+        <br />
+        <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+          blanditiis architecto quasi impedit in dicta nisi, asperiores
+          voluptatum eos alias facilis assumenda ex beatae, culpa dignissimos
+          accusantium quod numquam dolores! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+          blanditiis architecto quasi impedit in dicta nisi, asperiores
+          voluptatum eos alias facilis assumenda ex beatae, culpa dignissimos
+          accusantium quod numquam dolores!
+        </p>
+        <br />
+        <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+          blanditiis architecto quasi impedit in dicta nisi, asperiores
+          voluptatum eos alias facilis assumenda ex beatae, culpa dignissimos
+          accusantium quod numquam dolores! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+          blanditiis architecto quasi impedit in dicta nisi, asperiores
+          voluptatum eos alias facilis assumenda ex beatae, culpa dignissimos
+          accusantium quod numquam dolores!
+        </p>
+        <br />
+        <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+          blanditiis architecto quasi impedit in dicta nisi, asperiores
+          voluptatum eos alias facilis assumenda ex beatae, culpa dignissimos
+          accusantium quod numquam dolores! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+          blanditiis architecto quasi impedit in dicta nisi, asperiores
+          voluptatum eos alias facilis assumenda ex beatae, culpa dignissimos
+          accusantium quod numquam dolores!
+        </p>
+      </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Policy;
