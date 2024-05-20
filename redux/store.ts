7e43b50c4b60c318f9b3360dbb1ed43d62090c61
vav <vavvav@i.ua>
2024-05-20 20:54:19 +0300

    slice

--- redux/store.ts
+++ redux/store.ts
@@ -2,9 +2,13 @@
 import { configureStore } from "@reduxjs/toolkit";
             import { setupListeners } from '@reduxjs/toolkit/query'
   import { apiSlice } from "./features/api/apiSlice";
- 
+  import authSlice from "./features/auth/authSlice";
+
+
 export const store = configureStore({
-  reducer: {    [apiSlice.reducerPath]: apiSlice.reducer,    },
+  reducer: {    [apiSlice.reducerPath]: apiSlice.reducer, 
+    auth: authSlice,  // слайс аутентификации
+  },
 
   devTools: true, 
   middleware: (getDefaultMiddleware) =>
