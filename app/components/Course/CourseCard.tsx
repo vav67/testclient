521856ea59dff2fa841821e385399cf1439efe92
vav <vavvav@i.ua>
2024-05-21 21:42:58 +0300

    course

--- app/components/Course/CourseCard.tsx
+++ app/components/Course/CourseCard.tsx
@@ -16,9 +16,9 @@ const CourseCard: FC<Props> = ({ item, isProfile }) => {
   return (
     <>
   
-    {/* <Link
+    <Link
        href={!isProfile ? `/course/${item._id}` : `course-access/${item._id}`}
-     > */}
+     >  
        <div className="w-full min-h-[35vh] dark:bg-slate-500 dark:bg-opacity-20 
  backdrop-blur border dark:border-[#ffffff1d] border-[#00000015] 
  dark:shadow-[bg-slate-700] rounded-lg p-3 shadow-sm dark:shadow-inner">
@@ -64,7 +64,7 @@ const CourseCard: FC<Props> = ({ item, isProfile }) => {
           </div> */}
         </div> 
       </div>
-      {/* </Link>  */}
+       </Link>   
     </>
   );
 };
